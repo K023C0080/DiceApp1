3d8548be1da4ddcffc56f5b5889c1caaca3461c5
K023C0080 <K023C0080@g.neec.ac.jp>
2024-12-19 13:31:10 +0900

    dekita

--- DiceApp/DiceApp2.java
+++ DiceApp/DiceApp2.java
@@ -1,1 +1,15 @@
 //test
+public class Game {
+    Player p1;
+    Player p2;
+    Dice d1;
+    Dice d2;
+
+    Game() {
+      p1 = new Player();
+      p2 = new Player();
+      d1 = new Dice();
+      d2 = new Dice();
+    }
+      
+}
---
+++ DiceApp/Game.java
@@ -0,0 +1,5 @@
+public class Game {
+  start()
+  judge()
+  
+}
